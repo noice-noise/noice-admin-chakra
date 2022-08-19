6af41f117ffa8e71a003b99a0dfa4a15a120c01c
Raul Comeros, Jr <90462635+noice-noise@users.noreply.github.com>
2022-08-19 21:06:18 +0800

    feat: add header

---
+++ src/components/Elements/Header/Header.tsx
@@ -0,0 +1,139 @@
+import {
+  Avatar,
+  Flex,
+  Icon,
+  IconButton,
+  Link as ExternalLink,
+  Menu,
+  MenuButton,
+  MenuDivider,
+  MenuGroup,
+  MenuItem,
+  MenuList,
+  Portal,
+  Text,
+} from '@chakra-ui/react';
+import { BiBell } from 'react-icons/bi';
+import { DiGithubBadge } from 'react-icons/di';
+import { Link } from 'react-router-dom';
+import { ThemeModeToggle } from '../ThemeModeToggle';
+
+export const Header = () => {
+  return (
+    <Flex
+      direction="row"
+      position="fixed"
+      bg="foreground"
+      px={5}
+      m={0}
+      top={0}
+      left={0}
+      w="100vw"
+      overflowY="auto"
+      overflowX="hidden"
+      h={14}
+      justifyContent="flex-end"
+      alignItems="center"
+      borderBottomWidth="1px"
+    >
+      <IconButton
+        as={ExternalLink}
+        href="https://github.com/noice-noise/noice-admin-chakra"
+        isExternal
+        aria-label="Link to repo"
+        color="current"
+        rounded="full"
+        icon={<Icon boxSize="2rem" as={DiGithubBadge} />}
+      />
+
+      <ThemeModeToggle />
+
+      <Menu strategy="absolute">
+        <MenuButton>
+          <IconButton
+            aria-label="Open notifications"
+            color="current"
+            rounded="full"
+            icon={<Icon boxSize="1.5rem" as={BiBell} />}
+          />
+        </MenuButton>
+        <Portal>
+          <MenuList fontSize="sm" minW={80}>
+            <MenuItem>
+              <Flex direction="column" alignItems="start" maxW={80}>
+                <Text fontWeight="bold">Patrek</Text>
+                <Text opacity={0.8} size="sm">
+                  Lag nasad ang Diverge
+                </Text>
+              </Flex>
+            </MenuItem>
+            <MenuItem>
+              <Flex direction="column" alignItems="start" maxW={80}>
+                <Text fontWeight="bold">MVPrinch</Text>
+                <Text opacity={0.8} size="sm">
+                  SURPASS YOUR LIMITS
+                </Text>
+              </Flex>
+            </MenuItem>
+            <MenuItem>
+              <Flex direction="column" alignItems="start" maxW={80}>
+                <Text fontWeight="bold">Junix</Text>
+                <Text opacity={0.8} size="sm">
+                  useEffect hihi
+                </Text>
+              </Flex>
+            </MenuItem>
+            <MenuItem>
+              <Flex direction="column" alignItems="start" maxW={80}>
+                <Text fontWeight="bold">Keanue Leeves</Text>
+                <Text opacity={0.8} size="sm">
+                  better than the real one
+                </Text>
+              </Flex>
+            </MenuItem>
+          </MenuList>
+        </Portal>
+      </Menu>
+
+      <Menu>
+        <MenuButton ml={4}>
+          <Avatar
+            name="User"
+            boxSize="2.5rem"
+            rounded="full"
+            src="https://bit.ly/broken-link"
+          />
+        </MenuButton>
+        <Portal>
+          <MenuList fontSize="sm" minW={40} w={20}>
+            <MenuGroup fontSize="sm" title="Profile">
+              <MenuItem as={Link} to="/account">
+                My Account
+              </MenuItem>
+              <MenuItem as={Link} to="/settings">
+                Settings
+              </MenuItem>
+            </MenuGroup>
+            <MenuDivider />
+            <MenuGroup fontSize="sm" title="Help">
+              <MenuItem
+                as={ExternalLink}
+                href="https://github.com/noice-noise/noice-admin-chakra"
+                isExternal
+              >
+                Documentation
+              </MenuItem>
+              <MenuItem
+                as={ExternalLink}
+                href="https://github.com/noice-noise/noice-admin-chakra/issues"
+                isExternal
+              >
+                Feedback
+              </MenuItem>
+            </MenuGroup>
+          </MenuList>
+        </Portal>
+      </Menu>
+    </Flex>
+  );
+};
--- src/components/Elements/ThemeModeToggle.tsx
+++ src/components/Elements/ThemeModeToggle.tsx
@@ -1,11 +1,28 @@
-import { Button, useColorMode } from '@chakra-ui/react';
+import { Icon, IconButton, Tooltip, useColorMode } from '@chakra-ui/react';
+import { BiMoon, BiSun } from 'react-icons/bi';
 
-export const ThemeModeToggle = () => {
+export const ThemeModeToggle = ({ ...props }) => {
   const { colorMode, toggleColorMode } = useColorMode();
 
   return (
-    <Button onClick={toggleColorMode}>
-      Toggle {colorMode === 'light' ? 'Dark' : 'Light'}
-    </Button>
+    <Tooltip
+      label={`Enable ${colorMode === 'light' ? 'Dark' : 'Light'} mode`}
+      placement="bottom"
+    >
+      <IconButton
+        color="current"
+        rounded="full"
+        aria-label={`Enable ${colorMode === 'light' ? 'Dark' : 'Light'} mode`}
+        onClick={toggleColorMode}
+        icon={
+          colorMode === 'light' ? (
+            <Icon as={BiMoon} boxSize="1.5rem" />
+          ) : (
+            <Icon as={BiSun} boxSize="1.5rem" />
+          )
+        }
+        {...props}
+      />
+    </Tooltip>
   );
 };

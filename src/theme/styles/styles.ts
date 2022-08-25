9c649349506ea2462a1657895fa460016430b8f8
Raul Comeros, Jr <90462635+noice-noise@users.noreply.github.com>
2022-08-25 17:53:06 +0800

    chore: return mode styles

--- src/theme/styles/styles.ts
+++ src/theme/styles/styles.ts
@@ -1,13 +1,12 @@
 import { StyleFunctionProps } from '@chakra-ui/react';
+import { mode } from '@chakra-ui/theme-tools';
 
 export const styles = {
   global: (props: StyleFunctionProps) => ({
     body: {
-      // fontFamily: 'body',
-      // color: mode('gray.800', 'whiteAlpha.900')(props),
-      // bg: mode('gray.100', 'gray.800')(props),
-      // lineHeight: 'base',
-      letterSpacing: 'wide',
+      fontFamily: 'body',
+      color: mode('gray.800', 'whiteAlpha.900')(props),
+      bg: mode('gray.100', 'gray.800')(props),
     },
   }),
 };
